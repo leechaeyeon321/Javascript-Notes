f7a43f588c50b1b7dbe9d1d19b646638d948c747
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-04-24 21:19:52 +0900

    feat: 01

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+let array = [3, 5, 1, 2, 4]; //5개의 데이터(N = 5)
+let summary = 0; //합계를 저장할 변수
+
+//모든 데이터를 하나씩 확인하며 합계를 계산
+for (let i = 0; i< array.length; i++){
+	summary += array[i];
+}
+
+//결과를 출력
+console.log(summary);
+
+let array2 = [3, 5, 1, 2, 4]; //5개의 데이터(N = 5)
+
+for(let i = 0; i< array2.length; i++){
+    for (let j = 0; i<array2.length; j++){
+        let temp = array2[i] * array2[j];
+        console.log(temp);
+    }
+}
